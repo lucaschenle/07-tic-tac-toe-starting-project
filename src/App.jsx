f60fb29c39b9baaf4aac410c0ecc54d54ce3ab39
lucaschenle <lucaschenle@gmail.com>
2024-01-22 20:38:49 +1100

    git push

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,21 @@
+import Player from "./components/Player";
 
 function App() {
-  return 
-  <main>
-    <div id="game-container">
-      PLAYERS
+  return (
+    <main>
+      <div id="game-container">
+        <ol id="players">
+          <Player initialName="Player 1" symbol="X" />
+          <Player initialName="Player 2" symbol="O" />
+        </ol>
 
-      GAME BOARD
 
-    </ div>
-  </main>
+        GAME BOARD
+        
+      </div>
+      LOG 
+    </main>
+  );
 }
 
 export default App
